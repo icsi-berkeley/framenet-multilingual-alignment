addfd44e200c8e5101d4e3d6733a53a6661cdcff
arthurlorenzi <lorenzi.arthur@gmail.com>
2020-04-06 21:26:29 -0300

    Updates

---
+++ precache-manifest.dafaed944e1b4b0339af02f9faa3a574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbb5fbafdc8bf682e5d8f98ce7f6409",
+    "url": "/framenet-multilingual-alignment/index.html"
+  },
+  {
+    "revision": "84212ed16b8606e6486f",
+    "url": "/framenet-multilingual-alignment/static/css/2.b31b2e03.chunk.css"
+  },
+  {
+    "revision": "277bfb2b63aad4ba7bf9",
+    "url": "/framenet-multilingual-alignment/static/css/main.b0d7f484.chunk.css"
+  },
+  {
+    "revision": "84212ed16b8606e6486f",
+    "url": "/framenet-multilingual-alignment/static/js/2.b02b9e61.chunk.js"
+  },
+  {
+    "revision": "1a2c6400532c174273c8cd4ad7b54815",
+    "url": "/framenet-multilingual-alignment/static/js/2.b02b9e61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "277bfb2b63aad4ba7bf9",
+    "url": "/framenet-multilingual-alignment/static/js/main.f706987a.chunk.js"
+  },
+  {
+    "revision": "15895c6a2fa8436860b7",
+    "url": "/framenet-multilingual-alignment/static/js/runtime-main.e05bbeec.js"
+  }
+]);
